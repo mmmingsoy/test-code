3b4990b8cfbee0ac5ad185229fcc8abb04366ed0
“mmmingsoy” <jiayi.ming@gl-inet.com>
2025-12-26 17:55:10 +0800

    提交代码

---
+++ package/utils/test-module/files/test.sh
@@ -0,0 +1,15 @@
+#!/bin/sh /etc/rc.common
+
+#最后执行
+START = 99
+
+#只在第一次启动运行
+[ -f "/etc/ssid-set-done" ] && exit 0
+
+#设置2.4g和5g WiFi的ssid
+uci set wireless.default_radio0.ssid='gl-inet'
+uci set wireless.default_radio1.ssid='gl-inet-5G'
+uci commit wireless
+
+touch /etc/ssid-set-done
+
